3f1d42d8449713f1358d00bf907617c8c675f7d0
MLusung <mtlusung@student.hau.edu.ph>
2025-06-24 16:02:49 +0800

    Landing page added

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import './LandingPage.css'
+import './LandingPage.css';
+import LoginPage from './LoginPage';
 
 function LandingPage() {
+  const [goTo, setGoTo] = useState(false);
+  const GoToLogin = (val) => {
+    setGoTo(true)
+  };
+
+  if(goTo){
+    return <LoginPage/>;
+  };
   return (
-    <div className='Container'>
-      <nav className='nav'>
-        <div className='NavWarpper'>
-            <a href="#!" class="brand-logo">MRTL</a>
-            <button className='loginbtn'>Login</button>
-        </div>
-      </nav>
-    </div>
+    <nav className='nav'>
+      <div className='navWarpper'>
+          <a href="#!" class="brand-logo">MRTL</a>
+          <div className='loginContainer'>
+            <button className='loginbtn' onClick={GoToLogin}>Login</button>
+          </div>
+      </div>
+    </nav>
   );
 }
 
